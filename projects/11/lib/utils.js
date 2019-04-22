f940a04de7a8c0572f788b308c9609313e5096b6
willclarktech <willclarktech@users.noreply.github.com>
2019-04-22 15:30:51 +0200

    WIP: fixup utils

--- projects/11/lib/utils.js
+++ projects/11/lib/utils.js
@@ -10,20 +10,20 @@ module.exports.makeEater = parsed => ([{ category, token }, ...tokens] = [], exp
 
 	const obj = { category, token };
 	parsed.push(obj)
-	if (process.env.DEBUG) console.log(parsed.length, 'category:', category, 'token:', token)
+	if (process.env.DEBUG_UTILS) console.log(parsed.length, 'category:', category, 'token:', token)
 	return tokens
 }
 
 module.exports.makeNester = parsed => nesting => {
 	const obj = { category: 'nest', nesting: nesting }
 	parsed.push(obj)
-	if (process.env.DEBUG) console.log(parsed.length, 'category:', obj.category, 'nesting:', nesting)
+	if (process.env.DEBUG_UTILS) console.log(parsed.length, 'category:', obj.category, 'nesting:', nesting)
 }
 
 module.exports.makeUnnester = parsed => nesting => {
 	const obj = { category: 'unnest', nesting: nesting }
 	parsed.push(obj)
-	if (process.env.DEBUG) console.log(parsed.length, 'category:', obj.category, 'nesting:', nesting)
+	if (process.env.DEBUG_UTILS) console.log(parsed.length, 'category:', obj.category, 'nesting:', nesting)
 }
 
 module.exports.makeNestingCompiler = (nest, unnest) => (nesting, compile) => tokens => {
